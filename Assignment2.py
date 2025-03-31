315a4ccc9c5715fc15cc818ef434efbfdb236952
ChristopherSoliman980 <cme.soliman@gmail.com>
2025-03-31 12:57:42 +0000

    I have completed the assignment #2

--- Assignment2.py
+++ Assignment2.py
@@ -86,10 +86,14 @@ def dot_product(list1, list2):
 
 def matrix_multiplication(matrix1, matrix2):
   
-    matrix1 = numpy.array(matrix1)
-    
-    matrix2 = numpy.array(matrix2)
+    result = [
+        [0 for _ in range(len(matrix2[0]))]
+        for _ in range(len(matrix1))
+        ]
 
-    result = matrix1 @ matrix2 
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
 
-    return result.tolist()
+    return result 
